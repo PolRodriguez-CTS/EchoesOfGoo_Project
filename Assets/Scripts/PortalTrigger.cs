acf6c98491360bd5cb62f84c35871df12b3b5cb7
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2026-05-06 23:51:10 +0200

    PreHawkTuah

--- Assets/Scripts/PortalTrigger.cs
+++ Assets/Scripts/PortalTrigger.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
-using System.Collections;
 
 public class PortalTrigger : MonoBehaviour
 {
     private Animator _animator;
+    private bool _yaSeActivo = false; // El cerrojo de seguridad
 
     void Awake()
     {
         _animator = GetComponent<Animator>();
     }
 
-    void OnTriggerEnter(Collider collider)
+    void OnTriggerEnter(Collider other)
     {
-        if(collider.gameObject.tag == "Player")
+        // 1. Comprobamos que sea el Player
+        // 2. Comprobamos que no esté ya abierto
+        if (other.CompareTag("Player") && !_yaSeActivo)
         {
-            _animator.SetTrigger("Build");
+            _yaSeActivo = true; // Cerramos el paso para que no se repita
+            
+            // Usamos SetTrigger como tenías, pero ahora solo pasará UNA vez
+            _animator.SetTrigger("Build"); 
+
+            Debug.Log("¡Portal activado!");
         }
     }
 }
